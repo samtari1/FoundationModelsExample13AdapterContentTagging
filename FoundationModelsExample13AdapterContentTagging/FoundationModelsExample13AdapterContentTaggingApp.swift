610168b428c14e8cabbe17cd68b586dcb1d758f2
samtari1 <samtariyang@gmail.com>
2026-03-05 12:26:03 -0500

    Initial Commit

---
+++ FoundationModelsExample13AdapterContentTagging/FoundationModelsExample13AdapterContentTaggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample13AdapterContentTaggingApp.swift
+//  FoundationModelsExample13AdapterContentTagging
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsExample13AdapterContentTaggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModelsExample13AdapterContentTaggingTests/FoundationModelsExample13AdapterContentTaggingTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample13AdapterContentTaggingTests.swift
+//  FoundationModelsExample13AdapterContentTaggingTests
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import Testing
+@testable import FoundationModelsExample13AdapterContentTagging
+
+struct FoundationModelsExample13AdapterContentTaggingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
